a02fb6177fe148af71813033f83a5146a0cc5f31
sideeq12 <abdwaheed2018@gmail.com>
2021-04-17 00:27:46 +0100

    ejs linked

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
+"use-strict;"
+
+const express = require("express");
+
+const app = express()
 
 
-// HiveOS boiler-plate code
-const baseUrl = 'https://api2.hiveos.farm/api/v2';
-let accessToken = null;
+app.listen(3030, ()=>{
+    console.log("Server running on port 3030")
+})
+// // HiveOS boiler-plate code
+// const baseUrl = 'https://api2.hiveos.farm/api/v2';
+// let accessToken = null;
 
-doLogin('your_login', 'your_password')
-    .then(getFarms)
-    .then(farms => console.log('farms=', farms));
+// doLogin('your_login', 'your_password')
+//     .then(getFarms)
+//     .then(farms => console.log('farms=', farms));
 
-function doLogin(login, password) {
-    return fetch(`${baseUrl}/auth/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({login, password})
-    }).then(r => {
-        if (!r.ok) {
-            r.json().then(data => {
-                console.error(data.message || 'Response error');
-            });
-            return Promise.reject(r);
-        }
-        else {
-            return r.json().then(data => {
-                accessToken = data.access_token;
-                return data;
-            });
-        }
-    })
-}
+// function doLogin(login, password) {
+//     return fetch(`${baseUrl}/auth/login`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({login, password})
+//     }).then(r => {
+//         if (!r.ok) {
+//             r.json().then(data => {
+//                 console.error(data.message || 'Response error');
+//             });
+//             return Promise.reject(r);
+//         }
+//         else {
+//             return r.json().then(data => {
+//                 accessToken = data.access_token;
+//                 return data;
+//             });
+//         }
+//     })
+// }
 
-function getFarms() {
-    return fetch(`${baseUrl}/farms`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-        }
-    }).then(r => {
-        if (!r.ok) {
-            r.json().then(data => {
-                console.error(data.message || 'Response error');
-            });
-            return Promise.reject(r);
-        }
-        else {
-            return r.json();
-        }
-    });
-}
+// function getFarms() {
+//     return fetch(`${baseUrl}/farms`, {
+//         method: 'GET',
+//         headers: {
+//             'Authorization': `Bearer ${accessToken}`,
+//         }
+//     }).then(r => {
+//         if (!r.ok) {
+//             r.json().then(data => {
+//                 console.error(data.message || 'Response error');
+//             });
+//             return Promise.reject(r);
+//         }
+//         else {
+//             return r.json();
+//         }
+//     });
+// }
